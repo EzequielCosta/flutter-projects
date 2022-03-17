f3b7b30b06365a53f43c1072bba4ea730008f787
Ezequiel Costa <ezequielteixeiracosta321@gmail.com>
2022-03-17 11:01:59 -0300

    Melhoria no main

--- slide_images/lib/main.dart
+++ slide_images/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -36,8 +34,8 @@ class SuperMario extends StatefulWidget {
 
 class _SuperMarioState extends State<SuperMario> {
   int _imgCurrent = 1;
-  //Map<int, int> images_liked = new Map();
-  var images_liked = {};
+  Map<int, int> images_liked = new Map();
+  //var images_liked = {};
   Color colorIcon = Colors.white;
 
   @override
@@ -52,13 +50,7 @@ class _SuperMarioState extends State<SuperMario> {
       setState(() {
         _imgCurrent++;
       });
-
-      if (images_liked.containsKey(_imgCurrent)) {
-        colorIcon = images_liked[_imgCurrent] == 1 ? Colors.red : Colors.white;
-      } else {
-        images_liked[_imgCurrent] = 0;
-        colorIcon = Colors.white;
-      }
+      getLike();
     }
   }
 
@@ -67,39 +59,64 @@ class _SuperMarioState extends State<SuperMario> {
       setState(() {
         _imgCurrent--;
       });
-
-      if (images_liked.containsKey(_imgCurrent)) {
-        colorIcon = images_liked[_imgCurrent] == 1 ? Colors.red : Colors.white;
-      } else {
-        images_liked[_imgCurrent] = 0;
-        colorIcon = Colors.white;
-      }
+      getLike();
     }
   }
 
-  Widget buildIconButton(Function function, IconData icon,
-      [Color colorIcon = Colors.teal]) {
-    return IconButton(
-        iconSize: 50,
-        onPressed: () {
-          function();
-        },
-        icon: Icon(
-          icon,
-          color: colorIcon,
-          size: 50,
-        ));
+  void getLike() {
+    if (images_liked.containsKey(_imgCurrent)) {
+      colorIcon = images_liked[_imgCurrent] == 1 ? Colors.red : Colors.white;
+    } else {
+      images_liked[_imgCurrent] = 0;
+      colorIcon = Colors.white;
+    }
   }
+
+  void _switchColorIconLike() {
+    setState(() {
+      colorIcon = (colorIcon == Colors.white) ? Colors.red : Colors.white;
+    });
+  }
+
+  int _switchValueIconLike() {
+    int valueLike = 1;
+    if (images_liked.containsKey(_imgCurrent)) {
+      valueLike = images_liked[_imgCurrent] == 0 ? 1 : 0;
+    }
+
+    return valueLike;
+  }
+
+  void _saveLikeInImage() {
+    // 0 - not like; 1 - like
+
+    setState(() {
+      images_liked[_imgCurrent] = _switchValueIconLike();
+    });
+  }
+
+  Widget _buildIconButton(Function function, IconData icon,
+          [Color colorIcon = Colors.teal]) =>
+      IconButton(
+          iconSize: 50,
+          onPressed: () {
+            function();
+          },
+          icon: Icon(
+            icon,
+            color: colorIcon,
+            size: 50,
+          ));
 
   Widget _footerButtons() {
     return Container(
-        margin: EdgeInsets.only(bottom: 40),
+        margin: const EdgeInsets.only(bottom: 40),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            buildIconButton(previousImg, Icons.arrow_circle_left_sharp),
-            buildIconLike(),
-            buildIconButton(nextImg, Icons.arrow_circle_right_sharp),
+            _buildIconButton(previousImg, Icons.arrow_circle_left_sharp),
+            _buildIconLike(),
+            _buildIconButton(nextImg, Icons.arrow_circle_right_sharp),
           ],
         ));
   }
@@ -114,40 +131,17 @@ class _SuperMarioState extends State<SuperMario> {
     );
   }
 
-  Widget buildIconLike() {
+  Widget _buildIconLike() {
     return IconButton(
         iconSize: 40,
         onPressed: () {
-          switchColorIconLike();
-          saveLikeInImage();
+          _switchColorIconLike();
+          _saveLikeInImage();
         },
         icon: Icon(
           CupertinoIcons.heart_fill,
           color: colorIcon,
           size: 40,
         ));
-  }
-
-  void switchColorIconLike() {
-    setState(() {
-      colorIcon = colorIcon == Colors.white ? Colors.red : Colors.white;
-    });
-  }
-
-  int switchValueIconLike() {
-    int valueLike = 1;
-    if (images_liked.containsKey(_imgCurrent)) {
-      valueLike = images_liked[_imgCurrent] == 0 ? 1 : 0;
-    }
-
-    return valueLike;
-  }
-
-  void saveLikeInImage() {
-    // 0 - not like; 1 - like
-
-    setState(() {
-      images_liked[_imgCurrent] = switchValueIconLike();
-    });
   }
 }
